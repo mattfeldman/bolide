a7dd9b8e680994ff202c0a64e45f7410978e9e1b
Matt Feldman <decaprime@gmail.com>
2015-10-10 21:17:38 -0700

    move hue to local package

---
+++ packages/bolide-hue/package.js
@@ -0,0 +1,13 @@
+Package.describe({
+    name: 'bolide:hue',
+    description: 'Hue interaction library for bolide',
+    version: '0.0.1',
+    git: ''
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.0');
+    api.use(['ecmascript']);
+    api.addFiles('hue.js');
+    api.export('Hue', 'server');
+});

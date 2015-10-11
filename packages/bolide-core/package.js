f06c4bea2a92f391e672eb315b6e740153544b82
Matt Feldman <decaprime@gmail.com>
2015-10-10 21:50:59 -0700

    abstract core into package

---
+++ packages/bolide-core/package.js
@@ -0,0 +1,28 @@
+Package.describe({
+    name: 'bolide:core',
+    description: 'Core package for bolide',
+    version: '0.0.1',
+    git: ''
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.0');
+
+    api.use([
+        'mongo',
+        'check',
+        'aldeed:simple-schema',
+        'audit-argument-checks',
+        'dburles:collection-helpers',
+        'ecmascript',
+        'bolide:hue'
+    ]);
+
+    api.addFiles('models/Bridges.js');
+    api.addFiles('models/Lights.js');
+
+    api.addFiles('server/bridgeMethods.js');
+    api.addFiles('server/lightMethods.js');
+
+    api.export(['Bridges', 'Lights']);
+});

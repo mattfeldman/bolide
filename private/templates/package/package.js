98139aae35cdb091984eab398b13855ec1c51579
Matt Feldman <decaprime@gmail.com>
2015-10-10 21:19:20 -0700

    update local package scaffold

--- private/templates/package/package.js
+++ private/templates/package/package.js
@@ -1,6 +1,7 @@
 /* { "path" : "packages/__namespace__-__packageName__/package.js" } */
 Package.describe({
     name: '__namespace__:__packageName__',
+    description: 'A local package for __namespace__',
     version: '0.0.1',
     git: ''
 });
@@ -9,13 +10,9 @@ Package.onUse(function(api) {
     api.versionsFrom('1.0');
 
     api.use([
-        'templating'
+        'ecmascript'
     ]);
 
     api.addFiles('__packageName__.js');
     api.export('__packageName__');
-});
-
-Package.onTest(function(api) {
-
 });

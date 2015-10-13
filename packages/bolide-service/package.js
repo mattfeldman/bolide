2518c9479df12c1df33fa26df8caf06ceb9102ac
Matt Feldman <decaprime@gmail.com>
2015-10-12 18:11:28 -0700

    move service to package

---
+++ packages/bolide-service/package.js
@@ -0,0 +1,21 @@
+Package.describe({
+    name: 'bolide:service',
+    description: 'Exposes v1 background service',
+    version: '0.0.1',
+    git: ''
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.0');
+
+    api.use([
+        'ecmascript',
+        'bolide:log',
+        'bolide:hue',
+        'bolide:core',
+    ]);
+
+    api.addFiles('service.js');
+    api.export('service', 'server');
+    api.addFiles('state-manager.js', 'server');
+});
--- packages/bolide-service/service.js
+++ packages/bolide-service/service.js
@@ -1,22 +1,30 @@
-hueService = {};
+service = {};
+let hueService = {};
+service.hue = function(){
+  return hueService || false;
+};
 let interval;
 function processBridge(bridge){
     hueService = {};
     if (!bridge) return;
     hueService = new Hue(bridge.ip, bridge.username);
-    console.log(hueService.baseUrl);
     if(interval){
         Meteor.clearInterval(interval);
     }
     interval = Meteor.setInterval(function () {
-        mapLights(hueService.getLights());
+        try {
+            mapLights(hueService.getLights());
+        }
+        catch(e){
+            Meteor.log.error(e);
+        }
     }, 5000);
 }
 function mapLights(rawResponse){
     if(!rawResponse) return;
     for(let key of Object.keys(rawResponse)){
         const light = rawResponse[key];
-        Lights.upsert(key, light);
+        Lights.upsert(key, {$set:{raw:light}});
     }
 }
 Meteor.startup(function() {

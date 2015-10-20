dc93023f6815ddd7173f831781b0f1485288edcd
Matt Feldman <decaprime@gmail.com>
2015-10-19 20:10:40 -0700

    rescuetime plugin

---
+++ packages/bolide-rescue-time/package.js
@@ -0,0 +1,17 @@
+Package.describe({
+    name: 'bolide:rescue-time',
+    description: 'A local package for bolide',
+    version: '0.0.1',
+    git: ''
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.0');
+    api.use([
+        'ecmascript',
+        'bolide:core',
+        'bolide:log',
+        'aramk:tinycolor'
+    ]);
+    api.addFiles('rescue-time.js','server');
+});
---
+++ packages/bolide-rescue-time/rescue-time.js
@@ -0,0 +1,18 @@
+Meteor.startup(function() {
+    let lightState = new LightState("rescue-time",3000);
+    Meteor.setInterval(function(){
+        const key = process.env.rescuetime_key;
+        if(!key) throw new Exception("Set env.rescuetime_key");
+        try {
+            const reqString = "https://www.rescuetime.com/anapi/data?key="+key+"&perspective=interval&rs=day&restrict_kind=efficiency&format=json";
+            const data = HTTP.get(reqString);
+            const prodPercent = data.data.rows[0][4];
+            const color = tinycolor.mix("#F00", "#00F", prodPercent);
+            const {r,g,b} = color.toRgb();
+            lightState.setLight(1, {rgb: {r, g, b}});
+        }
+        catch(e){
+            // nada
+        }
+    },15000);
+});7

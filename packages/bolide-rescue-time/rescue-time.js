deec1113f1d87797f2f81fed89c6af98fb49067e
Matt Feldman <decaprime@gmail.com>
2015-10-21 20:37:28 -0700

    Throw javascript error

--- packages/bolide-rescue-time/rescue-time.js
+++ packages/bolide-rescue-time/rescue-time.js
@@ -2,7 +2,7 @@ Meteor.startup(function() {
     let lightState = new LightState("rescue-time",3000);
     Meteor.setInterval(function(){
         const key = process.env.rescuetime_key;
-        if(!key) throw new Exception("Set env.rescuetime_key");
+        if(!key) throw new Error("Set env.rescuetime_key");
         try {
             const reqString = "https://www.rescuetime.com/anapi/data?key="+key+"&perspective=interval&rs=day&restrict_kind=efficiency&format=json";
             const data = HTTP.get(reqString);
@@ -15,4 +15,4 @@ Meteor.startup(function() {
             // nada
         }
     },15000);
-});7
+});

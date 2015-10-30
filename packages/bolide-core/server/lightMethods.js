b0a30a262e7141a872f8f140ef71a10866c572f8
Matt Feldman <decaprime@gmail.com>
2015-10-30 09:19:33 -0700

    re-enable latency comp on light server methods

--- packages/bolide-core/server/lightMethods.js
+++ packages/bolide-core/server/lightMethods.js
@@ -1,13 +1,11 @@
 const manual = new ManualState();
 Meteor.methods({
     setLightOn(id, on) {
-        if(!Meteor.isServer) return;
         check(id, String);
         check(on, Boolean);
         manual.setLight(id, {on: on});
     },
     setLightColor(id, r, g, b) {
-        if(!Meteor.isServer) return;
         check(id, String);
         check(r, Number);
         check(g, Number);
@@ -15,11 +13,9 @@ Meteor.methods({
         manual.setLight(id, {rgb: {r,g,b}});
     },
     setLightBrightness(id, brightness){
-        if(!Meteor.isServer) return;
         check(id, String);
         check(brightness, Number);
         manual.setLight(id, {bri: brightness});
-
     },
     setLightRandom(id){
         if(!Meteor.isServer) return;

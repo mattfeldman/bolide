e80b432f4c3523ae515ab44f96feaeaec265f9b6
Matt Feldman <decaprime@gmail.com>
2015-10-30 09:19:19 -0700

    className

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,7 +1,7 @@
 Footer = React.createClass({
     render(){
         return (
-            <div class="ui inverted center aligned padding-wrapper footer segment">
+            <div className="ui inverted center aligned padding-wrapper footer segment">
                 <a href="https://github.com/mattfeldman/bolide"><i class="github icon"></i> Bolide</a> by
                 <a href="https://github.com/mattfeldman">Matt Feldman</a>
             </div>

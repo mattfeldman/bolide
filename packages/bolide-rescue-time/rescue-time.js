eefb8ea9deb3fa7dc5250be48058c92b1bdfff60
Matt Feldman <decaprime@gmail.com>
2015-11-11 19:05:17 -0800

    make rescue time plugin reactive to config

--- packages/bolide-rescue-time/rescue-time.js
+++ packages/bolide-rescue-time/rescue-time.js
@@ -1,17 +1,22 @@
 Meteor.startup(function() {
     let lightState = new LightState("rescue-time",3000);
-    const settings = PluginSettings.findOne("settings");
-    if(!settings || !settings.key) return;
     Meteor.setInterval(function(){
         try {
+            const settings = PluginSettings.findOne("rescueTime");
+            if(!settings || !settings.key) return;
+            if(!settings.enabled){
+                lightState.clearState(settings.light);
+                return;
+            }
             const reqString = "https://www.rescuetime.com/anapi/data?key="+settings.key+"&perspective=interval&rs=day&restrict_kind=efficiency&format=json";
             const data = HTTP.get(reqString);
             const prodPercent = data.data.rows[0][4];
             const color = tinycolor.mix("#F00", "#00F", prodPercent);
             const {r,g,b} = color.toRgb();
-            lightState.setLight(1, {rgb: {r, g, b}});
+            lightState.setLight(settings.light, {rgb: {r, g, b}});
         }
         catch(e){
+            console.log(e);
             // nada
         }
     }, 15000);

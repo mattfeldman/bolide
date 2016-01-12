40c3b125224326bd0b59ee5cb4f1a5dc927673cb
Matt Feldman <decaprime@gmail.com>
2016-01-11 17:24:49 -0800

    rescue time logging

--- packages/bolide-rescue-time/rescue-time.js
+++ packages/bolide-rescue-time/rescue-time.js
@@ -1,4 +1,6 @@
+let log = new BolideLog({category: "plugin", module:"rescuetime"});
 Meteor.startup(function() {
+    log.info("Started");
     let lightState = new LightState("rescue-time",3000);
     Meteor.setInterval(function(){
         try {
@@ -12,12 +14,13 @@ Meteor.startup(function() {
             const data = HTTP.get(reqString);
             const prodPercent = data.data.rows[0][4];
             const color = tinycolor.mix("#F00", "#00F", prodPercent);
+            log.info(`Productivity ${prodPercent*100} resulting in ${color.toRgbString()}`);
             const {r,g,b} = color.toRgb();
             lightState.setLight(settings.light, {rgb: {r, g, b}});
         }
         catch(e){
-            console.log(e);
-            // nada
+            log.info(`Error ${e.ToString()}`);
+
         }
     }, 15000);
 });
